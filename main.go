a557c10317205e4812841690e2222d23792af09d
ShimiTal <shimon.tal@sap.com>
2025-10-25 12:20:12 +0300

    Fix nil-pointer at main.go:41

--- main.go
+++ main.go
@@ -37,7 +37,10 @@ func calcHandler(w http.ResponseWriter, r *http.Request) {
     }
     parts := strings.Split(numsParam, ",")
     idx, _ := strconv.Atoi(indexStr)
-    // BUG: no bounds check
+    if idx < 0 || idx >= len(parts) {
+        http.Error(w, "index out of range", http.StatusBadRequest)
+        return
+    }
     n, _ := strconv.Atoi(parts[idx])
     w.Header().Set("Content-Type", "application/json")
     _, _ = w.Write([]byte(fmt.Sprintf(`{"value":%d}`, n)))
@@ -64,5 +67,4 @@ func main() {
     log.Printf("faulty-app listening on %s", addr)
     log.Fatal(http.ListenAndServe(addr, mux))
 }
-
 
